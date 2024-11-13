9fc055ed80ddea44b210e8079d0f43f495761ee1
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-11-13 08:23:11 +0200

    icons a positions

--- src/components/welcome-page/WelcomePage.jsx
+++ src/components/welcome-page/WelcomePage.jsx
@@ -39,6 +39,16 @@ const WelcomePage = () => {
           }`}
           onClick={() => setUserType("Employee")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-person-lock"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 5.996V14H3s-1 0-1-1 1-4 6-4q.845.002 1.544.107a4.5 4.5 0 0 0-.803.918A11 11 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664zM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1" />
+          </svg>
           Employee
         </button>
         <button
@@ -47,6 +57,16 @@ const WelcomePage = () => {
           }`}
           onClick={() => setUserType("Guest")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-person"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+          </svg>
           Guest
         </button>
       </div>
