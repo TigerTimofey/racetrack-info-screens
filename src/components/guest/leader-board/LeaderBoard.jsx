e2e0d2a56ccfea9e21f7a241cc7a8603b0801901
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-11-29 12:14:00 +0200

    leader board -> fastest time shows from top speed & lap times shows after timer

--- src/components/guest/leader-board/LeaderBoard.jsx
+++ src/components/guest/leader-board/LeaderBoard.jsx
@@ -2,8 +2,16 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 import { backButton } from "../../../assets/button/buttons";
+import Timer from "../../timer/Timer";
+import "./LeaderBoard.css";
 
 const socket = io("http://localhost:3000/fast");
+const flagOptions = [
+  { name: "Safe", color: "#2ecc71" },
+  { name: "Hazard", color: "#f1c40f" },
+  { name: "Danger", color: "#e74c3c" },
+  { name: "Finish", isChequered: true }, // Add a flag for chequered
+];
 
 const LeaderBoard = () => {
   const navigate = useNavigate();
@@ -11,6 +19,19 @@ const LeaderBoard = () => {
   const [responseMessage, setResponseMessage] = useState("");
   const [responseData, setResponseData] = useState(null);
   const [showLaps, setShowLaps] = useState(false);
+  const [raceEnded, setRaceEnded] = useState(false);
+
+  //**************************************** COMES FROM SOCKET ****************************************************
+  const [currentFlag, setCurrentFlag] = useState(flagOptions[0]); // Default to "Safe"
+  const [currentRace, setCurrentRace] = useState(
+    "Racename from SAFETY OFFICIAL socket"
+  );
+
+  const handleSetCurrentRace = () => {
+    setCurrentRace(); // WHAT WILL COME FROM SOCKET
+  };
+
+  // ***********************************************************************************************************
 
   useEffect(() => {
     socket.emit("findAllFastestLap");
@@ -25,6 +46,7 @@ const LeaderBoard = () => {
     timerSocket.on("message", (msg) => {
       if (msg === "Timer finished") {
         setShowLaps(true);
+        setCurrentFlag(flagOptions[3]);
       }
     });
 
@@ -39,49 +61,98 @@ const LeaderBoard = () => {
         {backButton}
       </div>
       <h2 className="front-title">Leader Board Interface</h2>
-      <p>Record lap crossings here</p>
-
-      {responseMessage && (
-        <div className="response-message">
-          <p>Server Response: {responseMessage}</p>
-        </div>
-      )}
 
       {responseMessage === "OK" && responseData && (
-        <div className="response-data">
-          <h3>Fastest Laps Leaderboard</h3>
-          <div>
-            <h4>Fastest Laps Data</h4>
-            <ul>
-              {responseData.fastestLapsData &&
-                responseData.fastestLapsData.map((lap, index) => (
-                  <li key={index}>
-                    <strong>
-                      Car {lap.carNumber} - Driver: {lap.driverName}
-                    </strong>
-                    :{" "}
-                    {Number.isFinite(lap.lapTime) && lap.lapTime > 0
-                      ? `${String(Math.floor(lap.lapTime / 60)).padStart(
-                          2,
-                          "0"
-                        )}:${String(Math.floor(lap.lapTime % 60)).padStart(
-                          2,
-                          "0"
-                        )} (${((lap.lapTime % 1) * 1000)
-                          .toFixed(0)
-                          .padStart(3, "0")} ms)`
-                      : "On the way"}
-                  </li>
-                ))}
-            </ul>
-          </div>
-          {showLaps && (
-            <div>
-              <h4>Passing Lap Data - SEE ONLY WHEN RACE IS ENDED!</h4>
-              <pre>{JSON.stringify(responseData.passingLapData, null, 2)}</pre>
+        <>
+          <div className="race-name">
+            <h2 className="race-name-title">{`${currentRace}`} </h2>
+            <div
+              className={`flag-box ${
+                currentFlag.isChequered ? "flag-box--chequered" : ""
+              }`}
+              style={{
+                backgroundColor: currentFlag.isChequered
+                  ? "transparent"
+                  : currentFlag.color,
+                display: "inline-block",
+                marginRight: "10px",
+              }}
+              title={currentFlag.name}
+            >
+              {currentFlag.isChequered && (
+                <div className="flag-box__pattern"></div>
+              )}
             </div>
-          )}
-        </div>
+          </div>
+
+          <div className="response-data">
+            <div className="fastest-lap-card">
+              <h4>Fastest Laps</h4>
+              <ul>
+                {responseData.fastestLapsData &&
+                  responseData.fastestLapsData.map((lap, index) => {
+                    let lapTimeToDisplay;
+
+                    if (lap.lapTime === "00:00") {
+                      lapTimeToDisplay = "00:00";
+                    } else if (typeof lap.lapTime === "string") {
+                      const [minutes, seconds] = lap.lapTime
+                        .split(":")
+                        .map(Number);
+                      const lapTimeInMilliseconds =
+                        (minutes * 60 + seconds) * 1000;
+
+                      lapTimeToDisplay = `${String(
+                        Math.floor(lapTimeInMilliseconds / 60000)
+                      ).padStart(2, "0")}:${String(
+                        Math.floor((lapTimeInMilliseconds % 60000) / 1000)
+                      ).padStart(2, "0")}`;
+                    } else if (typeof lap.lapTime === "number") {
+                      const lapTimeInMilliseconds = lap.lapTime * 1000;
+                      lapTimeToDisplay = `${String(
+                        Math.floor(lapTimeInMilliseconds / 60000)
+                      ).padStart(2, "0")}:${String(
+                        Math.floor((lapTimeInMilliseconds % 60000) / 1000)
+                      ).padStart(2, "0")}`;
+                    } else if (lap.lapTime === null) {
+                      lapTimeToDisplay = "On the way";
+                    }
+
+                    return (
+                      <li key={index}>
+                        <strong>
+                          Car {lap.carNumber} - Driver: {lap.driverName}
+                        </strong>
+                        : {lapTimeToDisplay}
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+
+            {/* Passing Laps Card */}
+            {!showLaps && <Timer onTimerFinish={() => setRaceEnded(true)} />}
+
+            {showLaps && responseData.passingLapData && (
+              <div className="passing-lap-card">
+                <h4>Passing Laps</h4>
+                {Object.entries(
+                  responseData.passingLapData.reduce((grouped, lap) => {
+                    const key = `Car ${lap.carNumber} - Driver: ${lap.driverName}`;
+                    if (!grouped[key]) grouped[key] = [];
+                    grouped[key].push(`Lap ${lap.lapNumber}: ${lap.lapTime}`);
+                    return grouped;
+                  }, {})
+                ).map(([key, laps], index) => (
+                  <div key={index} className="lap-details">
+                    <h5>{key}</h5>
+                    <p>{laps.join(" | ")}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </>
       )}
 
       {responseMessage !== "OK" && responseMessage && (
